fa484f73f9a0315645ec52bde1905c162444159e
cyx <vsuh@mail.ru>
2023-05-01 16:32:17 +0300

    password auto substitution added

--- commit.cmd
+++ commit.cmd
@@ -1,9 +1,10 @@
 @echo off
 chcp 65001>nul
-set /p pw="Пароль для архива: "
+set /p pw= < password.hiddenfile
 set /p cm="Текст коммита: "
 
 rar u -p%pw% rclone_conf.rar rclone.conf
+
 git add .
 git commit -am "%cm%"
 git push
