a0b2bc61ae93b95d0ecf060e30f8791458704126
cyx <vsuh@mail.ru>
2023-12-07 21:08:42 +0300

    scripts minor chng

--- commit.cmd
+++ commit.cmd
@@ -4,7 +4,7 @@ chcp 65001>nul
 set /p pw= < password.hiddenfile
 if '%*'=='' (set /p cm="Текст коммита: ") ELSE (set cm=%*)
 
-rar u -hp%pw% rclone_conf.rar rclone.conf
+rar u -hp%pw% rclone_conf.rar rclone.conf *.cmd
 
 git add .
 git commit -am "%cm%"
