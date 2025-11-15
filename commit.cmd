d4f0aedc3a545174d83ba8a859748beb391f3660
cyx <vsuh@mail.ru>
2025-11-15 13:59:11 +0300

    removed section ml5

--- commit.cmd
+++ commit.cmd
@@ -1,10 +1,13 @@
 @echo off
+setlocal enabledelayedexpansion
 chcp 65001>nul
 
 set /p pw= < password.hiddenfile
 if '%*'=='' (set /p cm="Текст коммита: ") ELSE (set cm=%*)
+set cm=%cm:"=%
 
 rar u -hp%pw% rclone_conf.rar rclone.conf *.cmd
+
 
 git add .
 git commit -am "%cm%"
